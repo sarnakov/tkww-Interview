ea33b7df9e3209bccbc9a4caf47a9381dea70ff4
Alex Sarnakov <a.sarnakov@gmail.com>
2022-07-06 13:21:26 +0200

    Image: add propTypes

--- src/components/UI/Image.js
+++ src/components/UI/Image.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 function Image(props) {
 	// Would be good to add suspence and fallback
 	return (
@@ -11,5 +11,10 @@ function Image(props) {
 		/>
 	);
 }
-
+Image.propTypes = {
+	src: PropTypes.string.isRequired,
+	className: PropTypes.string,
+	width: PropTypes.number,
+	alt: PropTypes.string,
+};
 export default Image;
