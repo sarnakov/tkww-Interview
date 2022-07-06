85f7b121cb90ac9f969c2d6f4d2a6435621c2453
Alex Sarnakov <a.sarnakov@gmail.com>
2022-07-06 13:24:01 +0200

    api: fix no-anonymous-default-export warning

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default {
-    getProduct: function(){
-        return axios.get("https://qa-registry-interview-api.regsvcs.theknot.com/products");
-    }
-}
+const API = {
+	getProduct: function () {
+		return axios.get(
+			'https://qa-registry-interview-api.regsvcs.theknot.com/products',
+		);
+	},
+};
+
+export default API;
